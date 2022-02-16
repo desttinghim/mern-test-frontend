30d12749481661d45c67b88ad0a616285d5ba5f8
Louis Pearson <louispearson@librem.one>
2022-02-16 15:19:41 -0700

    Add UserItem

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Schwarz",
+      image: "https://place-hold.it/300",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
