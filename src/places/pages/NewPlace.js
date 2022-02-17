2ade76d09c573137667212d62c01e9b8401d49d8
Louis Pearson <louispearson@librem.one>
2022-02-17 12:40:10 -0700

    Add validators

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+
 import "./NewPlace.css";
 
 const NewPlace = () => {
@@ -10,7 +12,7 @@ const NewPlace = () => {
         element="input"
         type="text"
         label="Title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
       />
     </form>
