f7d571e5fe2b1d76c5a490d120f086f5c3b9126c
Louis Pearson <louispearson@librem.one>
2022-02-17 15:32:07 -0700

    Add auth context

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+
+import { AuthContext } from "../../context/auth-context";
 
 import "./NavLinks.css";
 
 const NavLinks = (props) => {
+  const authCtx = useContext(AuthContext);
   return (
     <ul className="nav-links">
       <li>
@@ -11,15 +14,21 @@ const NavLinks = (props) => {
           ALL USERS
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/places">MY PLACES</NavLink>
-      </li>
-      <li>
-        <NavLink to="/places/new">ADD PLACE</NavLink>
-      </li>
-      <li>
-        <NavLink to="/auth">AUTHENTICATE</NavLink>
-      </li>
+      {authCtx.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
+        </li>
+      )}
+      {authCtx.isLoggedIn && (
+        <li>
+          <NavLink to="/places/new">ADD PLACE</NavLink>
+        </li>
+      )}
+      {!authCtx.isLoggedIn && (
+        <li>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
+        </li>
+      )}
     </ul>
   );
 };
